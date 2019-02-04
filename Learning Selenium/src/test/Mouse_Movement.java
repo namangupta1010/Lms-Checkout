63f5b9d627d82c26e29653d064b4070bd408aee0
namangupta1010 <namangupta1010@gmail.com>
2019-02-04 18:40:55 +0530

    monday commit

--- Learning Selenium/src/test/Mouse_Movement.java
+++ Learning Selenium/src/test/Mouse_Movement.java
@@ -1,5 +1,8 @@
 package test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
@@ -8,25 +11,45 @@ import org.openqa.selenium.interactions.Actions;
 
 public class Mouse_Movement {
 
-	public static void main(String[] args) throws InterruptedException {
+	public static void main(String[] args) throws InterruptedException{
 		// TODO Auto-generated method stub
-		System.setProperty("webdriver.chrome.driver","E:\\Selenium\\chromedriver_win32\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver","D:\\\\Naman\\\\Selenium\\\\chromedriver_win32\\\\chromedriver.exe");
 		WebDriver c=new ChromeDriver();
 		c.manage().window().maximize();
 		c.get("https://www.proposalways.com/userLogin");
 		c.findElement(By.id("user_email")).sendKeys("naman.gupta@webners.com");
 		c.findElement(By.id("user_password")).sendKeys("abc123");
 		c.findElement(By.id("loginButton")).click();
-		Thread.sleep(5000);
+		Thread.sleep(3000);
 		c.findElement(By.id("arrowsidenavbar")).click();
 		
 		Actions d = new Actions(c);
-		d.moveToElement(c.findElement(By.linkText("Sections"))).build().perform();
+		d.moveToElement(c.findElement(By.linkText("Proposals"))).build().perform();
 		Thread.sleep(2000);
-		c.findElement(By.linkText("New")).click();
+		c.findElement(By.linkText("View")).click();
+		
+		Thread.sleep(2000);
+		c.findElement(By.xpath("//a[@href='sendproposalpages/S6999N']")).click();
 		
 		
-		c.switchTo().frame(0);
+		String date ="2019-02-04";
+		String datearr[] =date.split("-");
+		for(String print : datearr)
+			System.out.println(print);
+		
+		c.findElement(By.xpath("//th[contains(text(),'September 2019')]")).click();
+		
+		
+		
+		
+		
+		
+		
+		c.close();
+		
+		
+		
+		/*c.switchTo().frame(0);
 		Thread.sleep(5000);
 		
 		c.findElement(By.xpath("/html[1]/body[1]/div[16]/div[1]/div[1]"))
@@ -38,15 +61,15 @@ public class Mouse_Movement {
 				+ "Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software"
 				+ " like Aldus PageMaker including versions of Lorem Ipsum.");
 		c.findElement(By.xpath("/html[1]/body[1]")).sendKeys(Keys.ENTER);
-		
+		*/
 		/*
 		d.clickAndHold(c.findElement(By.xpath("//*[@id='draggable']")))
 		.moveToElement(c.findElement(By.xpath("//*[@id='droppable']")))
 		.release().build().perform();*/
-
+/*
 		c.findElement(By.id("showMatrix")).click();
 		c.findElement(By.id("btn_2_3")).click();
-		
+		*/
 	}
 
 }
