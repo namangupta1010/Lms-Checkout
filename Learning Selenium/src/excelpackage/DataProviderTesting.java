87f3d103de86da8985ef6f0e775df78fcbed6b80
namangupta1010 <namangupta1010@gmail.com>
2019-02-10 17:40:31 +0530

    Little changes

--- Learning Selenium/src/excelpackage/DataProviderTesting.java
+++ Learning Selenium/src/excelpackage/DataProviderTesting.java
@@ -12,38 +12,38 @@ import org.testng.annotations.Test;
 public class DataProviderTesting 
 {
 	WebDriver c;
-	 
-	  @BeforeMethod
-	  public void BrowserLaunch()
-	  {
-		  System.setProperty("webdriver.chrome.driver","F:\\naman QA\\Selenium\\chromedriver win32\\chromedriver.exe");
-		  c= new ChromeDriver();
-		  c.get("https://www.proposalways.com/userLogin");
-		  c.manage().window().maximize();
-		  c.manage().deleteAllCookies();
-	  }
+
+	@BeforeMethod
+	public void BrowserLaunch()
+	{
+		System.setProperty("webdriver.chrome.driver","F:\\naman QA\\Selenium\\chromedriver win32\\chromedriver.exe");
+		c= new ChromeDriver();
+		c.get("https://www.proposalways.com/userLogin");
+		c.manage().window().maximize();
+		c.manage().deleteAllCookies();
+	}
 	@Test(dataProvider="proposalways Data")
-	public void ProposalwaysLogin(String email,String Password,String un)
+	public void ProposalwaysLogin(String email,String Password)
 	{
 		c.findElement(By.id("user_email")).sendKeys(email);
 		c.findElement(By.id("user_password")).sendKeys(Password);
-		System.out.println(un);
 		
+
 	}
-	
+
 	@DataProvider(name ="proposalways Data")	
 	public Object[][] testData() throws IOException 
 	{
-		
+
 		Generic_dataDriven obj = new Generic_dataDriven("C:\\Users\\HP\\git\\Lms-Checkout\\Learning Selenium\\src\\excelpackage\\Proposalways.xlsx","Login Credentials");
 		Object data[][]=obj.getData();
 		return data;
-		
+
 	}
 
 	@AfterMethod
-	  public void logout() 
-	 {
-		 c.close();
-	 }
+	public void logout() 
+	{
+		c.close();
+	}
 }
