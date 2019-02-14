d16f8248cae91caa5db800e2c97c53952be391a5
namangupta1010 <namangupta1010@gmail.com>
2019-02-14 18:33:22 +0530

    thursday commit

--- Learning Selenium/src/test/Link_Text.java
+++ Learning Selenium/src/test/Link_Text.java
@@ -1,7 +1,12 @@
 package test;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.xmlbeans.impl.xb.xsdschema.All;
 import org.openqa.selenium.By;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 //import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -9,13 +14,32 @@ public class Link_Text {
 
 	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
-		System.setProperty("webdriver.chrome.driver","D:\\Naman\\Selenium\\chromedriver_win32\\chromedriver");
+		System.setProperty("webdriver.chrome.driver","D:\\Naman\\Selenium\\chromedriver_win32\\chromedriver.exe");
 		WebDriver c=new ChromeDriver();
-		c.get("https://naman1.lmscheckouttest.com/Course/index");
-		Thread.sleep(5000);
-		 String att= c.findElement(By.partialLinkText("Account")).getAttribute("class");
-		System.out.println(att);
+		c.get("https://www.studysection.com/");
+
+		List<WebElement> AllLinksandImages    =		c.findElements(By.xpath("//a"));
+
+		AllLinksandImages.addAll(c.findElements(By.xpath("//img")));
+
+		System.out.println(AllLinksandImages.size());
 		
+		
+		ArrayList<WebElement> activeLinks = new ArrayList<WebElement>();
+		activeLinks.addAll(AllLinksandImages);
+		System.out.println(activeLinks.size());
+
+		for(int i=0;i<AllLinksandImages.size();i++)
+		{
+
+
+			if(AllLinksandImages.get(i).getAttribute("href")!= null)
+			{
+				activeLinks.add(AllLinksandImages.get(i));
+			}
+		}
+		System.out.println(activeLinks.size());
+		c.close();	
 	}
 
 }
